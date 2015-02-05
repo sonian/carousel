e747280794821cc79ca4f2f532beeadfda3b65a0
Joe Gallo <joegallo@gmail.com>
2015-02-05 12:53:50 -0500

    Back to development

--- project.clj
+++ project.clj
@@ -12,7 +12,7 @@
 ;;   See the License for the specific language governing permissions and
 ;;   limitations under the License.
 
-(defproject sonian/carousel "1.0.4"
+(defproject sonian/carousel "1.0.5-SNAPSHOT"
   :description "A simple module system."
   :url "https://github.com/sonian/carousel"
   :license {:name "Apache 2.0"
